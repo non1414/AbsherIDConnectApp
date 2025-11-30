400c31bd09be59b17cffde0a081a1c70028f1254
non1414 <nouf22228@gmail.com>
2025-11-30 07:43:18 +0300

    Initial Commit

---
+++ AbsherIDConnectApp/AbsherIDConnectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsherIDConnectAppApp.swift
+//  AbsherIDConnectApp
+//
+//  Created by نوف بخيت الغامدي on 09/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct AbsherIDConnectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbsherIDConnectAppTests/AbsherIDConnectAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AbsherIDConnectAppTests.swift
+//  AbsherIDConnectAppTests
+//
+//  Created by نوف بخيت الغامدي on 09/06/1447 AH.
+//
+
+import Testing
+@testable import AbsherIDConnectApp
+
+struct AbsherIDConnectAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
