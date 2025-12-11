0feb768ae554611c7a890ba85c130b9f8ab19472
non1414 <nouf22228@gmail.com>
2025-12-11 18:24:20 +0300

    ..

---
+++ AbsherIDConnectApp/AbsherConnectDemoView.swift
@@ -0,0 +1,8 @@
+//
+//  AbsherConnectDemoView.swift
+//  AbsherIDConnectApp
+//
+//  Created by نوف بخيت الغامدي on 09/06/1447 AH.
+//
+
+import Foundation
---
+++ AbsherIDConnectApp/SplashVideoDemoView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashVideoDemoView.swift
+//  AbsherIDConnectApp
+//
+//  Created by نوف بخيت الغامدي on 10/06/1447 AH.
+//
+
+import Foundation
